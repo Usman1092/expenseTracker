429f9d3e5863ef7a89293bbed01dcd9162a09853
Usman Ali <usman23485@gmail.com>
2024-11-16 09:29:10 +0500

    Ok

--- vite.config.js
+++ vite.config.js
@@ -9,3 +9,21 @@ export default defineConfig({
     chunkSizeWarningLimit: 1600,
   },
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// export default defineConfig({
+//   base: "/Stakepool-Frontend/",
+//   plugins: [react()],
+//   resolve: {
+//   alias: {
+//   "~": path.resolve(__dirname, "node_modules"),
+//   "@": path.resolve(__dirname, "src"),
+//   },
+//   },
+//   build: {
+//   chunkSizeWarningLimit: 1600,
+//   },
+//   });
